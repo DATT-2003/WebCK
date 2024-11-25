c8712a446cf75c3a76d189fae3ab2896f2781f4d
Jishakami <lequangbaongan806@gmail.com>
2024-11-25 16:52:40 +0700

    Ngan

--- client/src/components/routers/AuthRouter.tsx
+++ client/src/components/routers/AuthRouter.tsx
@@ -1,17 +1,32 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Search, Edit, Booking } from '../screen'
+import React from "react";
+import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
+import { Search, Edit, Booking } from "../screen";
+import SearchBar from "../SearchFunctionComponents/SearchBar";
+
+const Searchroom = () => {
+  return (
+    <div>
+      {/* SearchBar chỉ xuất hiện khi truy cập đường dẫn /search */}
+      <SearchBar />
+      <Outlet /> {/* Hiển thị các component con dựa trên route */}
+    </div>
+  );
+};
 const AuthRouter = () => {
-    return (
-        <div className="col content-center">
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/' element={<Edit />} />
-                    <Route path='/booking' element={<Booking />} />
-                    <Route path='/search' element={<Search />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    )
-}
-export default AuthRouter
+  return (
+    <div className="col content-center">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Edit />} />
+          <Route path="/booking" element={<Booking />} />
+
+          <Route path="/search" element={<Searchroom />}>
+            {/* Thành phần cụ thể cho /search */}
+            <Route index element={null} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+export default AuthRouter;
